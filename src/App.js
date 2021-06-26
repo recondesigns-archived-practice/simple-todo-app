31282d5b399c65be9f4a40a29023ae7ab528881b
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-06-26 02:52:40 -0500

    Added routing
    Added pages and routing.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
-
+import { Switch, Route } from 'react-router-dom'
 import HomePage from './pages/Home'
+import SignupPage from './pages/Signup'
+import DashboardPage from './pages/Dashboard'
 
 export default function App() {
   return (
     <>
-      <HomePage />
+      <Switch>
+        <Route exact path={'/'} component={HomePage} />
+        <Route exact path={'/signup'} component={SignupPage} />
+        <Route exact path={'/dashboard'} component={DashboardPage} />
+      </Switch>
     </>
   )
 }
