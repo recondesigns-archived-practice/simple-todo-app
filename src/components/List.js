db9356d47df6885cb6cb83adc7e21d44b5b1deab
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-06-28 16:34:48 -0500

    Commit

--- src/components/List.js
+++ src/components/List.js
@@ -7,26 +7,59 @@ const Container = styled.div`
     display: flex;
     flex-direction: column;
     overflow-y: scroll;
-    // border: 2px solid lightgray;
+    border: 2px solid lightgray;
 `
 
-const Item = styled.p`
+const ItemContainer = styled.div`
     margin-top: 8px;
-    margin-bottom: 8px;
-    padding: 8px 0px 8px 0px;
+    // padding: 8px 0px 8px 0px;
+    border: 1px dashed green;
+`
+
+const TaskName = styled.p`
+    padding: 8px 0px;
     font-family: 'Merriweather', serif;
     font-weight: 400;
+    font-size: 16px;
+    line-height: 24px;
     color: #414141;
     text-align: center;
-    // border: 1px dashed green;
+    border: 1px dotted pink;
+    opacity: ${props => props.opacity};
+    text-decoration: ${props => props.textDecoration};
 `
 
 export default function List(props) {
     const { listItems } = props
 
+    function setCompletedStyles(status) {
+        if (status === true) {
+            return {
+                opacity: '0.2',
+                textDecoration: 'line-through'
+            }
+        } else if (status === false) {
+            return {
+                opacity: '1',
+                textDecoration: 'none'
+            }
+        }
+    }
+
     let itemsList = listItems.map((userTask, idx) => {
-        const { task } = userTask
-        return <Item key={idx}>{task}</Item>
+        const { task, isComplete } = userTask
+        const { opacity, textDecoration } = setCompletedStyles(isComplete)
+
+        return (
+            <ItemContainer key={idx}>
+                <TaskName 
+                    opacity={opacity} 
+                    textDecoration={textDecoration}
+                >
+                    {task}
+                </TaskName>
+            </ItemContainer>
+        )
     })
 
     return (
